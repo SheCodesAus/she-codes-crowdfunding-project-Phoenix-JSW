407ac78623a0e65f0ba86229938e052357c3452f
Phoenix-JSW <jswylds@gmail.com>
2022-04-26 16:01:45 +0800

    React Crowdfunding base

---
+++ src/data.js
@@ -0,0 +1,25 @@
+// export const allProjects = [
+//     {
+//         id: 1,
+//         title: "Project One",
+//         description: "The first project.",
+//         goal: 150,
+//         image: "https://via.placeholder.com/300.jpg",
+//         is_open: false,
+//         date_created: "2020-03-20T14:22:23.382748Z",
+//         owner: 1,
+//     },
+// ...
+// ]
+
+// export const oneProject = {
+//     id: 1,
+//     title: "Project One",
+//     description: "The first project.",
+//     goal: 150,
+//     image: "https://via.placeholder.com/300.jpg",
+//     is_open: false,
+//     date_created: "2020-03-20T14:22:23.382748Z",
+//     owner: 1,
+//     pledges: [
+//         {
